d1d09ea10c295e49251be491c52ade4671063cad
apersic <apersic@foi.hr>
2020-10-15 15:47:52 +0200

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="LetsTalk">Let's talk!</h1>
+    </div>
+  );
+}
+
+export default App;
