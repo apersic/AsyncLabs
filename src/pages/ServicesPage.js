57cb4fdf9477182e9cf00bfbd17e8ab96eac0540
apersic <apersic@foi.hr>
2020-10-20 01:15:30 +0200

    Services design update

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -41,7 +41,7 @@ function ServicesPage() {
             <button className="View-service-button">View service</button>
           </div>
         </div>
-        <div className="Service1">
+        <div className="Service2">
           <div className="Service-content">
             <h2 className="Service-name">Software & App Development</h2>
             <p className="Service-description">
@@ -99,6 +99,49 @@ function ServicesPage() {
             <button className="View-service-button">View service</button>
           </div>
         </div>
+      </div>
+      <div className="Banner-procces">
+        <div className="Procces-wrapper">
+          <div className="Procces">
+            <div className="Procces-image">
+              <img src={require("../assets/1(1).png")} alt="Procces 1"></img>
+            </div>
+            <h3 className="Procces-name">Analysis & Idea</h3>
+            <span className="Procces-description">
+              Branding and digital are forte, given the state of the world,
+              think.
+            </span>
+          </div>
+        </div>
+        <div className="Procces-wrapper">
+          <div className="Procces">
+            <div className="Procces-image">
+              <img src={require("../assets/02.png")} alt="Procces 1"></img>
+            </div>
+            <h3 className="Procces-name">Design & Development</h3>
+            <span className="Procces-description">
+              Dominion it air bearing subdue seed very wherein, moving seas.
+            </span>
+          </div>
+        </div>
+        <div className="Procces-wrapper">
+          <div className="Procces">
+            <div className="Procces-image">
+              <img src={require("../assets/3.png")} alt="Procces 1"></img>
+            </div>
+            <h3 className="Procces-name">Testing & Launch</h3>
+            <span className="Procces-description">
+              Land very had you and god make void forth said all and greater.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="Banner-get-in-touch">
+        <h2 className="Get-in-touch-title">
+          Interested in discussing a project?
+        </h2>
+        <p className="Get-in-touch-description"></p>
+        <button className="Get-in-touch-button">Get in touch</button>
       </div>
     </div>
   );
