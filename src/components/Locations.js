7eec6f1f209d76e12b498e31f4ae7eb2869f7184
apersic <apersic@foi.hr>
2020-10-21 01:21:23 +0200

    Added responsive design

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -3,18 +3,24 @@ import React from "react";
 function Locations() {
   return (
     <div className="Locations-container">
-      <div className="Location">
-        <h1>London, UK</h1>
-        <p>Vlaška ulica, 70 E hello@asynclabs.co +385 99 4936 071</p>
-      </div>
-      <div className="Location-zagreb">
-        <h1>Zagreb, Croatia</h1>
-        <p>Vlaška ulica, 70 E hello@asynclabs.co +385 99 4936 071</p>
-      </div>
-      <div className="Location-zagreb-black">
-        <h1>Zagreb, Croatia</h1>
-        <p>Lipovečka, 1 hello@asynclabs.co +385 99 4936 071</p>
-      </div>
+      <a href="#">
+        <div className="Location">
+          <h1>London, UK</h1>
+          <p>Vlaška ulica, 70 E hello@asynclabs.co +385 99 4936 071</p>
+        </div>
+      </a>
+      <a href="#">
+        <div className="Location-zagreb">
+          <h1>Zagreb, Croatia</h1>
+          <p>Vlaška ulica, 70 E hello@asynclabs.co +385 99 4936 071</p>
+        </div>
+      </a>
+      <a href="#">
+        <div className="Location-zagreb-black">
+          <h1>Zagreb, Croatia</h1>
+          <p>Lipovečka, 1 hello@asynclabs.co +385 99 4936 071</p>
+        </div>
+      </a>
     </div>
   );
 }
